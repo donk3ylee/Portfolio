8e66157d06a03296a60662dfcab488cc7016d70f
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2021-09-28 17:44:43 +0100

    Added small pop out menu at lower screensizes. Added a real page to code examples. Changed styles to creat more white space overall.

--- JS/main.js
+++ JS/main.js
@@ -6,15 +6,20 @@ sliderTrigger.addEventListener("click", function(e){
     // document.getElementById("slider-container").classList.add('slider-active');
     document.getElementById("slider-container").style.display = "flex";
     document.getElementById("x").style.display = "flex";
+    document.getElementById('slider-trigger').style.display = "none";
 });
 
 window.addEventListener("resize", function(e){
     if(window.innerWidth > 771){
         document.getElementById("slider-container").style.display = "none";
+        document.getElementById("slider-trigger").style.display = "none";
+    }
+    else{
+        document.getElementById("slider-trigger").style.display = "flex";
     }
 });
 
 sliderCloser.addEventListener("click", function(e){
     document.getElementById("slider-container").style.display = "none";
-    // document.getElementById("slider-container").classList.add('slider-container');
+    document.getElementById('slider-trigger').style.display = "flex";
 });
