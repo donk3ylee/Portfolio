b85cfaedcad12d5e1e0693de56b5770e190b30ee
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2021-11-18 17:06:18 +0000

    Added an accordion to the coding examples page and created another section uisng an SQL example

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,10 @@
-// Sliding menu code start
+// START - Sliding menu
 const sliderTrigger = document.getElementById('slider-trigger');
 const sliderCloser = document.getElementById('x');
 const sliderContainer = document.getElementById('slider-container');
 
 
 sliderTrigger.addEventListener("click", (e) => {
-    // sliderContainer.classList.add('slider-active');
-    console.log('trigger;')
     sliderContainer.style.display = "flex";
     sliderCloser.style.display = "flex";
     sliderTrigger.style.display = "none";
@@ -26,3 +24,31 @@ sliderCloser.addEventListener("click", (e) => {
     sliderContainer.style.display = "none";
     sliderTrigger.style.display = "flex";
 });
+// END - Sliding menu
+
+
+// START - Accordion for coding_examples page.
+const accordionItem = document.querySelectorAll('.accordion');
+const accordionTitle = document.querySelectorAll('.accordion .accordion-title');
+const accordionBody = document.querySelectorAll('.accordion .accordion-body');
+const accordionFooter = document.querySelectorAll('.accordion .accordion-footer');
+const accordionInstructions = document.querySelectorAll('.accordion .accordion-instructions');
+
+
+for (i = 0; i < accordionItem.length; i++) {
+    const title = accordionTitle[i]
+    const body = accordionBody[i]
+    const footer = accordionFooter[i]
+    const instruct = accordionInstructions[i]
+    title.addEventListener('click',() => {
+        body.classList.toggle('active')
+        footer.classList.toggle('active')
+        instruct.classList.toggle('contract-instructions')
+    })
+    footer.addEventListener('click',() => {
+        body.classList.toggle('active')
+        footer.classList.toggle('active')
+        instruct.classList.toggle('contract-instructions')
+    })
+}
+//  END - Accordion
